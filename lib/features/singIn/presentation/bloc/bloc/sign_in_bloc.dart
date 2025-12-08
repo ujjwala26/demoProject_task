a95e61ae1fc4fe6d042fc25111133cffc1feef80
ujjwala26 <kujjwala628@gmail.com>
2025-12-08 16:32:36 +0530

    update sign_in_bloc

--- lib/features/singIn/presentation/bloc/bloc/sign_in_bloc.dart
+++ lib/features/singIn/presentation/bloc/bloc/sign_in_bloc.dart
@@ -12,70 +12,7 @@ part 'sign_in_state.dart';
 class SignInBloc extends Bloc<SignInEvent, SignInState> {
   SignInBloc() : super(SignInInitial()) {
     on<SignInButtonPressed>(signInButtonPressed);
-    // on<SignInButtonPressed>((event, emit) async {
-    //   emit(SignInLoading()); 
-    //   try{
-    //         final response = await http.post(Uri.parse("https://fakestoreapi.com/auth/login"),
-    //         body: jsonEncode({
-    //           "username" : event.password,
-    //           //"email" : event.email,
-    //           "password" : event.username,
-    //         }),
-    //         headers: {"Content-Type": "application/json"},);
-    //         if(response.statusCode==201){
-    //           final data = jsonDecode(response.body);
-    //           final token=data["token"];
-    //           print("token : $token");
-    //           emit(SignInSuccess());
-
-    //         }else{
-    //           emit(SignInFailure("invalid credentials"));
-    //         }
-
-
-    //       }catch(e){
-    //         emit(SignInFailure(e.toString()));
-
-    //       }
-
-    //   // await Future.delayed(Duration(seconds: 1)); 
-
-    //   // if (event.email == "test@gmail.com" && event.password == "123456") {
-    //   //   emit(SignInSuccess());
-    //   // } else {
-    //   //   emit(SignInFailure("Invalid email or password"));
-    //   // }
-
-    //   Future<void> login(
-
-    //     SignInButtonPressed event , Emitter<SignInState>emit)async{
-    //       emit(SignInLoading());
-
-    //       try{
-    //         final response = await http.post(Uri.parse("https://fakestoreapi.com/auth/login"),
-    //         body: jsonEncode({
-    //           "username" : event.username,
-    //           //"email" : event.email,
-    //           "password" : event.password,
-    //         }),
-    //         headers: {"Content-Type": "application/json"},);
-    //         if(response.statusCode==200){
-    //           final data = jsonDecode(response.body);
-    //           final token=data["token"];
-    //           print("token : $token");
-    //           emit(SignInSuccess());
-
-    //         }else{
-    //           emit(SignInFailure("invalid credentials"));
-    //         }
-
-
-    //       }catch(e){
-    //         emit(SignInFailure(e.toString()));
-
-    //       }
-    //     }
-    // });
+    
   }
 
   Future<void> signInButtonPressed(SignInButtonPressed event, Emitter<SignInState> emit)async {
